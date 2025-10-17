3e7b20e2c818601b71fcd74132970438c64a52cd
HaydenA0 <aregrag2005@gmail.com>
2025-10-17 07:30:26 +0100

    added mvp of fft

---
+++ src/fft.py
@@ -0,0 +1,13 @@
+from scipy.fft import fft2, fftshift, ifft2
+import numpy as np
+
+
+class FoureirTransform:
+    def __init__(self) -> None:
+        pass
+
+    def process_image(self, image, filter_defined):
+        image_fft = fft2(image)
+        image_fft_filterd = image_fft * filter_defined
+        reverse_image = np.array(ifft2(image_fft_filterd))
+        return np.real(reverse_image)
--- src/fft_testing.py
+++ src/fft_testing.py
@@ -1,19 +1,17 @@
-from scipy.fft import fft2, fftshift
+from scipy.fft import fftshift
 import numpy as np
+from fft import FoureirTransform
 
 from utilsio import ImageIO
 from visulization import Visualizer
 
 io = ImageIO()
 vis = Visualizer()
+fafat = FoureirTransform()
 
 image = np.array(io.load_image("./images/grass.png", format="Grayscale"))
 
-image_fourier_amplitudes = np.abs(np.array(fft2(image)))
-visual_fourier_amplitudes = np.log(fftshift(image_fourier_amplitudes + 1))
+filter = np.ones_like(image)
+new_image = fafat.process_image(image, filter)
 
-
-vis.compare_images(
-    [image, visual_fourier_amplitudes],
-    ["Original Image", "$F(u,v)$ Fourier Transformation "],
-)
+vis.compare_images([image, new_image], ["Original", "Processed"])
